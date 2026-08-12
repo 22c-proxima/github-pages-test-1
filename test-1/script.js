168672c725b1ff7796bf0082c8c3e7a62fb1a770
Владимир Шомин <22c.proxima@gmail.com>
2026-08-13 01:32:53 +0200

    Test index 7

--- test-1/script.js
+++ test-1/script.js
@@ -1,8 +1,14 @@
-const sResult = document.getElementById("result"),
-  wUrl = "https://api.open-meteo.com/v1/forecast?latitude=44.804&longitude=20.4651&current_weather=true";
+const sResult = document.getElementById("result");
 
 document.getElementById("checkTemperature").addEventListener("click", function() {
-  fetch(wUrl)
+  sResult.textContent = "Получение данных...";
+  fetch('https://ipapi.co/json/').then(response => response.json())
+    .then(geo => {
+      const lat = geo.latitude;
+      const lon = geo.longitude;
+      const wUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`;
+      return fetch(wUrl);
+    })
     .then(response => response.json())
     .then(data => sResult.textContent = data.current_weather.temperature + "°C")
     .catch(error => {
